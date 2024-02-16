aab23ddbad2aebf8d7851156594844df53a76800
sabrina <sabri.cis1802@gmail.com>
2024-02-16 09:55:50 -0300

    Commit Inicial
    Commit Inicial

---
+++ src/main/java/org/acme/restjsonquickstart/RestJsonQuickstartApplication.java
@@ -0,0 +1,13 @@
+package org.acme.restjsonquickstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestJsonQuickstartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestJsonQuickstartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/acme/restjsonquickstart/RestJsonQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package org.acme.restjsonquickstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJsonQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
